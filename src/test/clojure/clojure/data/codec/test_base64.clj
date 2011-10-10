05034913da6ced1cfd396eb9662bba916b295bec
Alexander Taggart <mail@ataggart.ca>
2011-10-10 03:08:01 -0700

    Properly handle offset. Remove return type hinting due to a bug in clojure. Add some perf improvements. Add tests.

--- src/test/clojure/clojure/data/codec/test_base64.clj
+++ src/test/clojure/clojure/data/codec/test_base64.clj
@@ -1,5 +1,4 @@
 (ns clojure.data.codec.test-base64
-  (:refer-clojure :exclude [time])
   (:import org.apache.commons.codec.binary.Base64)
   (:use clojure.test
         clojure.data.codec.base64))
@@ -11,8 +10,19 @@
     (byte-array)))
 
 (deftest correctness
-  (doseq [n (range 1 129)]
+  (doseq [n (range 1 100)]
     (is (let [input (rand-bytes n)
               a1 (encode input)
               a2 (Base64/encodeBase64 input)]
           (= (into [] a1) (into [] a2))))))
+
+(deftest offset-correctness
+  (doseq [n (range 1 100)]
+    (doseq [off (range 1 n)]
+      (is (let [input (rand-bytes n)
+                len (- n off)
+                a1 (encode input off len)
+                input2 (byte-array len)
+                _ (System/arraycopy input off input2 0 len)
+                a2 (Base64/encodeBase64 input2)]
+            (= (into [] a1) (into [] a2)))))))
