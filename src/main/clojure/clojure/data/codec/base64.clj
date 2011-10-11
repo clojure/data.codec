bbb96996fd854e4e7d4d473d7661b9a234e80f64
Alexander Taggart <mail@ataggart.ca>
2011-10-11 02:22:56 -0700

    Add legal mumbo jumbo.

--- src/main/clojure/clojure/data/codec/base64.clj
+++ src/main/clojure/clojure/data/codec/base64.clj
@@ -1,4 +1,17 @@
-(ns clojure.data.codec.base64
+;; by Alex Taggart
+;; October 11, 2011
+
+;; Copyright (c) Alex Taggart, October 2011. All rights reserved.  The use
+;; and distribution terms for this software are covered by the Eclipse
+;; Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;; which can be found in the file epl.html at the root of this
+;; distribution.  By using this software in any fashion, you are
+;; agreeing to be bound by the terms of this license.  You must not
+;; remove this notice, or any other, from this software.
+
+(ns ^{:author "Alex Taggart"
+      :doc "Functions for working with base64 encodings."}
+  clojure.data.codec.base64
   (:import [java.io InputStream OutputStream]))
 
 (set! *unchecked-math* true)
