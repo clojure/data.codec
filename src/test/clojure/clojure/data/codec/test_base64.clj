1c1f03af3712d528426e3a5362833c9e153318a5
Alexander Taggart <ataggart@circleci.com>
2017-11-19 21:15:24 -0500

    Formatting only.

--- src/test/clojure/clojure/data/codec/test_base64.clj
+++ src/test/clojure/clojure/data/codec/test_base64.clj
@@ -2,7 +2,7 @@
   (:import org.apache.commons.codec.binary.Base64
            java.io.ByteArrayInputStream
            java.io.ByteArrayOutputStream)
-  (:require [clojure.test :refer [deftest is]] 
+  (:require [clojure.test :refer [deftest is]]
             [clojure.test.check :as tc]
             [clojure.test.check.clojure-test :refer [defspec]]
             [clojure.test.check.generators :as gen]
@@ -17,9 +17,9 @@
 
 (defn aeq?
   ([a b]
-    (= (seq a) (seq b)))
+   (= (seq a) (seq b)))
   ([a b len]
-    (= (take len a) (take len b))))
+   (= (take len a) (take len b))))
 
 (defn copy-bytes ^bytes [array offset length]
   (let [arr (byte-array length)]
@@ -29,15 +29,15 @@
 
 (defn expected-enc
   (^bytes [input]
-    (Base64/encodeBase64 input))
+   (Base64/encodeBase64 input))
   (^bytes [input offset length]
-    (Base64/encodeBase64 (copy-bytes input offset length))))
+   (Base64/encodeBase64 (copy-bytes input offset length))))
 
 (defn expected-dec
   (^bytes [^bytes input]
-    (Base64/decodeBase64 input))
+   (Base64/decodeBase64 input))
   (^bytes [input offset length]
-    (Base64/decodeBase64 (copy-bytes input offset length))))
+   (Base64/decodeBase64 (copy-bytes input offset length))))
 
 (defn gen-discrete [min max base]
   (let [min (quot min base)
@@ -46,17 +46,17 @@
 
 (defn gen-with-offset-length [gen-array step]
   (gen/bind gen-array
-    (fn [^bytes array]
-      (if (zero? (alength array))
-        (gen/tuple (gen/return array)
-                   (gen/return -1)
-                   (gen/return 0))
-        (gen/bind (gen-discrete 0 (dec (alength array)) step)
-                  (fn [len]
-                    (gen/tuple
-                      (gen/return array)
-                      (gen-discrete 0 (- (alength array) len) step)
-                      (gen/return len))))))))
+            (fn [^bytes array]
+              (if (zero? (alength array))
+                (gen/tuple (gen/return array)
+                           (gen/return -1)
+                           (gen/return 0))
+                (gen/bind (gen-discrete 0 (dec (alength array)) step)
+                          (fn [len]
+                            (gen/tuple
+                              (gen/return array)
+                              (gen-discrete 0 (- (alength array) len) step)
+                              (gen/return len))))))))
 
 (def gen-bytes-offset-length (gen-with-offset-length gen/bytes 1))
 
@@ -66,9 +66,9 @@
 
 (defspec check-encode!
   (prop/for-all [[^bytes input offset length] gen-bytes-offset-length]
-    (let [dest (byte-array (* 2 (alength input)))
+    (let [dest    (byte-array (* 2 (alength input)))
           enc-len (encode! input offset length dest)
-          exp (expected-enc input offset length)]
+          exp     (expected-enc input offset length)]
       (and (= enc-len (alength exp))
            (aeq? dest exp enc-len)))))
 
@@ -89,30 +89,30 @@
 
 (defspec check-decode!
   (prop/for-all [[^bytes input offset length] gen-encbytes-offset-length]
-		(let [dest (byte-array (alength input))
-		      dec-len (decode! input offset length dest)
-		      exp (expected-dec input offset length)]
-		  (and (= dec-len (alength exp))
-		       (aeq? dest exp dec-len)))))
+    (let [dest    (byte-array (alength input))
+          dec-len (decode! input offset length dest)
+          exp     (expected-dec input offset length)]
+      (and (= dec-len (alength exp))
+           (aeq? dest exp dec-len)))))
 
 (deftest test-decode!-prim
   (is (instance? clojure.lang.IFn$OLLOL decode!)))
 
 (defspec check-decode-1
   (prop/for-all [^bytes input gen-encbytes]
-		(let [dec (decode input)
-		      exp (expected-dec input)]
-		  (aeq? dec exp))))
+    (let [dec (decode input)
+          exp (expected-dec input)]
+      (aeq? dec exp))))
 
 (defspec check-decode-3
   (prop/for-all [[^bytes input offset length] gen-encbytes-offset-length]
-		(let [dec (decode input offset length)
-		      exp (expected-dec input offset length)]
-		  (aeq? dec exp))))
+    (let [dec (decode input offset length)
+          exp (expected-dec input offset length)]
+      (aeq? dec exp))))
 
 (defspec check-encoding-transfer-default-buffer-size
   (prop/for-all [input gen/bytes]
-    (let [in (ByteArrayInputStream. input)
+    (let [in  (ByteArrayInputStream. input)
           out (ByteArrayOutputStream.)]
       (encoding-transfer in out)
       (aeq? (.toByteArray out) (expected-enc input)))))
@@ -120,14 +120,14 @@
 (defspec check-encoding-transfer-buffer-size
   (prop/for-all [input gen/bytes
                  buffer-size (gen/fmap #(* 3 %) (gen/fmap inc gen/nat))]
-    (let [in (ByteArrayInputStream. input)
+    (let [in  (ByteArrayInputStream. input)
           out (ByteArrayOutputStream.)]
       (encoding-transfer in out :buffer-size buffer-size)
       (aeq? (.toByteArray out) (expected-enc input)))))
 
 (defspec check-encoding-transfer-bad-buffer-size
   (prop/for-all [input gen/bytes]
-    (let [in (ByteArrayInputStream. input)
+    (let [in  (ByteArrayInputStream. input)
           out (ByteArrayOutputStream.)]
       (try
         (encoding-transfer in out :buffer-size 2)
@@ -137,7 +137,7 @@
 
 (defspec check-decoding-transfer-default-buffer-size
   (prop/for-all [input gen-encbytes]
-    (let [in (ByteArrayInputStream. input)
+    (let [in  (ByteArrayInputStream. input)
           out (ByteArrayOutputStream.)]
       (decoding-transfer in out)
       (aeq? (.toByteArray out) (expected-dec input)))))
@@ -145,14 +145,14 @@
 (defspec check-decoding-transfer-buffer-size
   (prop/for-all [input gen-encbytes
                  buffer-size (gen/fmap #(* 4 %) (gen/fmap inc gen/nat))]
-    (let [in (ByteArrayInputStream. input)
+    (let [in  (ByteArrayInputStream. input)
           out (ByteArrayOutputStream.)]
       (decoding-transfer in out :buffer-size buffer-size)
       (aeq? (.toByteArray out) (expected-dec input)))))
 
 (defspec check-decoding-transfer-bad-buffer-size
   (prop/for-all [input gen-encbytes]
-    (let [in (ByteArrayInputStream. input)
+    (let [in  (ByteArrayInputStream. input)
           out (ByteArrayOutputStream.)]
       (try
         (decoding-transfer in out :buffer-size 2)
